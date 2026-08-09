2c3859c5e20e4e785c463c6852eefef2aa13916c
auxmeet <mit35826@gmail.com>
2026-08-10 07:13:05 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@ POSTFSDATA=false
 LATESTARTSERVICE=false
 print_modname() {
   ui_print " "
-  ui_print "========================================"
-  ui_print "= Fix brightness on Infinix Hot 12 pro ="
-  ui_print "========================================"
-  ui_print " This module has been maded by @auxmeet "
-  ui_print "       See my github repository         "
+  ui_print "========================="
+  ui_print "= Fix brightness on GSI ="
+  ui_print "========================="
+  ui_print "This module maded by @auxmeet"
+  ui_print "Also thanks to @ironemenero"
   ui_print " "
 }
